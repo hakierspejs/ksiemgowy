6ddc57194b9c78b97909b7b37c048829f678a50b
Jacek Wielemborek <d33tah@gmail.com>
2020-04-09 23:02:44 +0200

    Only synchronize if we have newer data.

--- homepage_updater.py
+++ homepage_updater.py
@@ -14,8 +14,14 @@ import ksiemgowy.public_state
 
 HOMEPAGE_REPO = 'hakierspejs/homepage'
 DUES_FILE_PATH = '_includes/dues.txt'
-PUBLIC_DB_URI = os.environ['PUBLIC_DB_URI']
 DUES_SEPARATOR = '\n\n{% comment %} END OF AUTOUPDATED PART {% endcomment %}'
+
+
+def parse_last_updated(s):
+    prefix = '{% assign dues_last_updated = "'
+    line = [l for l in s.split('\n') if l.startswith(prefix)][0]
+    date_s = line.split(prefix)[1].split('"')[0]
+    return datetime.datetime.strptime(date_s, '%d-%m-%Y')
 
 
 def get_local_state(db):
@@ -31,17 +37,18 @@ def get_local_state(db):
             last_updated = action['timestamp']
         num_subscribers += 1
         total += action['amount_pln']
-    last_updated = '09-04-2020'
+    last_updated_s = last_updated.strftime('%d-%m-%Y')
     return textwrap.dedent(f'''
         {{% assign dues_total_lastmonth = {total} %}}
-        {{% assign dues_last_updated = "{last_updated}" %}}
+        {{% assign dues_last_updated = "{last_updated_s}" %}}
         {{% assign dues_num_subscribers = {num_subscribers} %}}
-    ''').strip()
+    ''').strip(), last_updated
 
 
 def get_remote_state():
     with open(f'homepage/{DUES_FILE_PATH}') as f:
-        return f.read().split(DUES_SEPARATOR)
+        ret = f.read().split(DUES_SEPARATOR)
+    return ret[0], ret[1], parse_last_updated(''.join(ret))
 
 
 def ssh_agent_import_key_and_build_env_and_setup_git(deploy_key_path):
@@ -71,7 +78,7 @@ def git_cloned(deploy_key_path):
         env = ssh_agent_import_key_and_build_env_and_setup_git(deploy_key_path)
         git_url = f'git@github.com:{HOMEPAGE_REPO}.git'
         env['GIT_SSH_COMMAND'] = ' '.join([
-            'ssh', '-o', 'LogLevel=DEBUG', '-o', 'StrictHostKeyChecking=no',
+            'ssh', '-o', 'StrictHostKeyChecking=no',
             '-o', 'UserKnownHostsFile=/dev/null'
         ])
         subprocess.check_call(['git', 'clone', git_url, cwd], env=env)
@@ -96,10 +103,12 @@ def update_remote_state(new_state, env):
 
 
 def maybe_update(db, deploy_key_path):
+    time.sleep(600.0)
     with git_cloned(deploy_key_path) as git_env:
-        local_state = get_local_state(db)
-        remote_state, suffix = get_remote_state()
-        if remote_state != local_state:
+        local_state, last_updated_local = get_local_state(db)
+        remote_state, suffix, last_updated_remote = get_remote_state()
+        is_newer = last_updated_local > last_updated_remote
+        if remote_state != local_state and is_newer:
             new_state = ''.join([local_state, DUES_SEPARATOR, suffix])
             update_remote_state(new_state, git_env)
 
@@ -109,7 +118,7 @@ def main():
     deploy_key_path = os.environ['DEPLOY_KEY_PATH']
     state = ksiemgowy.public_state.PublicState(PUBLIC_DB_URI)
     maybe_update(state, deploy_key_path)
-    schedule.every().day.do(maybe_update, (state, deploy_key_path))
+    schedule.every().hour.do(maybe_update, (state, deploy_key_path))
     while True:
         time.sleep(1.0)
         schedule.run_pending()
