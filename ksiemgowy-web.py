9e6ca75df4defc7c4493d81ecacf8cb4e463cc70
Jacek Wielemborek <d33tah@gmail.com>
2020-09-21 12:38:03 +0200

    In ksiemgowy-web.py, also fix counting of people who paid dues.

--- ksiemgowy-web.py
+++ ksiemgowy-web.py
@@ -14,7 +14,18 @@ app = flask.Flask(__name__)
 @app.route('/liczba_placacych')
 def liczba_placacych():
     state = ksiemgowy.public_state.PublicState(PUBLIC_DB_URI)
-    return str(len(list(state.list_mbank_actions())))
+    observed_acc_numbers = set()
+    observed_acc_owners = set()
+    num_subscribers = 1  # because d33tah's transfers are invisible to system
+    for action in state.list_mbank_actions():
+        if (
+            action["in_acc_no"] not in observed_acc_numbers
+            and action["in_person"] not in observed_acc_owners
+        ):
+            num_subscribers += 1
+            observed_acc_numbers.add(action["in_acc_no"])
+            observed_acc_owners.add(action["in_person"])
+    return str(num_subscribers)
 
 
 if __name__ == '__main__':
