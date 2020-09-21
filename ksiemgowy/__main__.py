c119bf771b51dc92e8c664a7e47390e7415d3b5f
Jacek Wielemborek <d33tah@gmail.com>
2020-09-21 12:39:27 +0200

    Reformat

--- ksiemgowy/__main__.py
+++ ksiemgowy/__main__.py
@@ -22,8 +22,8 @@ import ksiemgowy.public_state
 
 
 IMAP_FILTER = '(SINCE "02-Apr-2020" FROM "kontakt@mbank.pl")'
-ACC_NO = '76561893'
-LOGGER = logging.getLogger('ksiemgowy.__main__')
+ACC_NO = "76561893"
+LOGGER = logging.getLogger("ksiemgowy.__main__")
 
 
 def imap_connect(login, password, server):
@@ -51,12 +51,12 @@ def gen_unseen_mbank_emails(db, mail):
     """Connects to imap_server using login and password from the arguments,
     then yields a pair (mail_id_as_str, email_as_eml_string) for each of
     e-mails coming from mBank."""
-    mail.select('inbox')
+    mail.select("inbox")
     _, data = mail.search(None, IMAP_FILTER)
     mail_ids = data[0]
     id_list = mail_ids.split()
     for mail_id in reversed(id_list):
-        _, data = mail.fetch(mail_id, '(RFC822)')
+        _, data = mail.fetch(mail_id, "(RFC822)")
         for n, response_part in enumerate(data):
             if not isinstance(response_part, tuple):
                 continue
@@ -64,26 +64,25 @@ def gen_unseen_mbank_emails(db, mail):
             mail_key = f'{msg["Date"]}_{n}'
             if db.was_imap_id_already_handled(mail_key):
                 continue
-            LOGGER.info('Handling e-mail id: %r', mail_id)
+            LOGGER.info("Handling e-mail id: %r", mail_id)
             yield msg
             db.mark_imap_id_already_handled(mail_key)
 
 
 def check_for_updates(
-    imap_login, imap_password, imap_server,
-    public_db_uri, private_db_uri
+    imap_login, imap_password, imap_server, public_db_uri, private_db_uri
 ):
     """Program's entry point."""
-    LOGGER.info('check_for_updates()')
+    LOGGER.info("check_for_updates()")
     public_state = ksiemgowy.public_state.PublicState(public_db_uri)
     private_state = ksiemgowy.private_state.PrivateState(private_db_uri)
     mail = imap_connect(imap_login, imap_password, imap_server)
     for msg in gen_unseen_mbank_emails(private_state, mail):
         parsed = ksiemgowy.mbankmail.parse_mbank_email(msg)
-        for action in parsed.get('actions', []):
-            LOGGER.info('Observed an action')
+        for action in parsed.get("actions", []):
+            LOGGER.info("Observed an action")
             is_acct_watched = action.out_acc_no == ACC_NO
-            if action.action_type == 'in_transfer' and is_acct_watched:
+            if action.action_type == "in_transfer" and is_acct_watched:
                 public_state.add_mbank_action(action.anonymized().asdict())
                 server = smtp_login(imap_login, imap_password)
                 send_mail(server, imap_login, imap_login, str(action))
@@ -92,26 +91,23 @@ def check_for_updates(
 
 
 def build_args():
-    public_db_uri = os.environ['PUBLIC_DB_URI']
-    private_db_uri = os.environ['PRIVATE_DB_URI']
-    imap_login = os.environ['IMAP_LOGIN']
-    imap_server = os.environ['IMAP_SERVER']
-    imap_password_path = os.environ['IMAP_PASSWORD_PATH']
+    public_db_uri = os.environ["PUBLIC_DB_URI"]
+    private_db_uri = os.environ["PRIVATE_DB_URI"]
+    imap_login = os.environ["IMAP_LOGIN"]
+    imap_server = os.environ["IMAP_SERVER"]
+    imap_password_path = os.environ["IMAP_PASSWORD_PATH"]
     imap_password = open(imap_password_path).read().strip()
-    return (
-        imap_login, imap_password, imap_server,
-        public_db_uri, private_db_uri
-    )
+    return (imap_login, imap_password, imap_server, public_db_uri, private_db_uri)
 
 
 @atexit.register
 def atexit_handler(*_, **__):
-    LOGGER.info('Shutting down')
+    LOGGER.info("Shutting down")
 
 
 def main():
-    logging.basicConfig(level='INFO')
-    LOGGER.info('ksiemgowyd started')
+    logging.basicConfig(level="INFO")
+    LOGGER.info("ksiemgowyd started")
     args = build_args()
     check_for_updates(*args)
     schedule.every().hour.do(check_for_updates, *args)
@@ -120,5 +116,5 @@ def main():
         time.sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
