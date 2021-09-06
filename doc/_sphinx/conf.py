0c8f4369f23bdeee577053dc10cc11190f36c55b
Jacek Wielemborek <d33tah@gmail.com>
2021-09-06 22:30:21 +0200

    Experiment: move files around so that html is in "doc" directory.

--- doc/_sphinx/conf.py
+++ doc/_sphinx/conf.py
@@ -1,19 +1,11 @@
 import sys
 
-sys.path.append('..')
-
+sys.path.append("../..")
 project = "ksiemgowy"
 copyright = "2021, Hakierspejs"
 author = "Hakierspejs"
-
-
 extensions = ["sphinx.ext.autodoc"]
-
 templates_path = ["_templates"]
-
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
 html_theme = "alabaster"
-
 html_static_path = ["_static"]
