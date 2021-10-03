341c416df7a85d9853959549f6f58d288af31d9c
Jacek Wielemborek <d33tah@gmail.com>
2021-10-03 20:05:47 +0200

    Add a sample e-mail file and the logic to return it.

--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,5 @@
+import email
+
 import unittest
 import unittest.mock as mock
 
@@ -22,16 +24,34 @@ class ScheduleMock:
 
 class EntrypointTestCase(unittest.TestCase):
 
-    def test_entrypoint_doesnt_crash(self):
+    def build_mail_mock(self, messages=None):
 
-        mail_search_mock = mock.Mock()
-        mail_search_mock.search.return_value = [None, ['']]
+        """Generates a mock that fakes imaplib interface, returning e-mails
+        from a given iterator. Not my proudest hack. Apologies!"""
+
+        def mail_fetch_mock(mail, _):
+            return None, [(None, mail)]
+
+        if messages is None:
+            messages = []
+        mail_connection_mock = mock.Mock()
+        messages_mock = mock.Mock()
+        messages_mock.split.return_value = messages
+        mail_connection_mock.search.return_value = (None, [messages_mock])
+        mail_connection_mock.fetch.side_effect = mail_fetch_mock
         mail_mock = mock.Mock()
-        mail_mock.imap_connect.return_value = mail_search_mock
+        mail_mock.imap_connect.return_value = mail_connection_mock
+
         smtp_login_mock = mock.Mock()
         smtp_login_mock.__enter__ = mock.Mock()
         smtp_login_mock.__exit__ = mock.Mock()
+
         mail_mock.smtp_login.return_value = smtp_login_mock
+
+        return mail_mock
+
+    def run_entrypoint(self, messages):
+        mail_mock = self.build_mail_mock(messages)
 
         config_mock = ksiemgowy_main.KsiemgowyConfig(
             database_uri='',
@@ -39,11 +59,19 @@ class EntrypointTestCase(unittest.TestCase):
             accounts=[
                 ksiemgowy_main.KsiemgowyAccount(
                     mail_config=mail_mock, acc_number='')
-            ], mbank_anonymization_key='')
+            ], mbank_anonymization_key=b'')
         database_mock = mock.Mock()
         database_mock.list_positive_transfers.return_value = []
+        database_mock.was_imap_id_already_handled.return_value = False
         ksiemgowy_main.main(config_mock, database_mock, mock.Mock(),
                             ScheduleMock(), lambda: False)
+
+    def test_entrypoint_doesnt_crash(self):
+
+        with open(
+                'docs/przykladowy_zalacznik_mbanku.eml', 'rb',
+        ) as f:
+            self.run_entrypoint([f.read()])
 
     def test_build_confirmation_mail_copies_email_if_not_in_mapping(self):
         mbank_action = MbankAction(
